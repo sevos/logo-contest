6f544eabd6bb92d64fe7bbf81058f91440b9fc3b
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-06-27 16:40:42 +0200

    Translations filtering in administration tool

--- app/controllers/administration/translations_controller.rb
+++ app/controllers/administration/translations_controller.rb
@@ -2,10 +2,27 @@ class Administration::TranslationsController < Administration::ApplicationContro
   resource_controller
   actions :all, :except => [:new, :create]
 
-  update.wants.html { redirect_to administration_translations_path }
+  before_filter :set_locale
+
+  update.wants.html { redirect_to administration_translations_path(:locale => params[:locale]) }
+  destroy.wants.html { redirect_to :back }
 
   def clean
-    Translation.not_translated.destroy_all
-    redirect_to administration_translations_path
+    Translation.not_translated.for_locale(params[:locale]).destroy_all
+    redirect_to administration_translations_path(params[:locale])
   end
+
+  private
+    def collection
+      @collection ||= if params[:locale]
+        Translation.for_locale(params[:locale])
+      else
+        super
+      end
+    end
+
+    def set_locale
+      @available_locales = Translation.available_locales
+      @current_locale = params[:locale]
+    end
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -5,10 +5,23 @@ class Translation < ActiveRecord::Base
   after_destroy { Translation.invalidate_cache }
 
   named_scope :not_translated, :conditions => {:translated => false}
+  named_scope :for_locale, lambda { |locale|
+    if locale
+      {
+        :conditions => {:locale => locale.to_s}
+      }
+    else
+      {}
+    end
+  }
 
   def self.invalidate_cache
     if cache_store = I18n.cache_store
       cache_store.delete_matched(/i18n-.*/)
     end
+  end
+
+  def self.available_locales
+    self.scoped(:select => "DISTINCT locale", :order => "locale ASC").map(&:locale)
   end
 end
