8c359bf37d1f1387034e4f65f028f4d3915251a9
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-06-27 12:17:53 +0200

    I18n translation caching with invalidation

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,6 +1,14 @@
 class Translation < ActiveRecord::Base
   default_scope :order => "created_at DESC"
   before_update { |r| r.translated = true }
+  after_save { Translation.invalidate_cache }
+  after_destroy { Translation.invalidate_cache }
 
   named_scope :not_translated, :conditions => {:translated => false}
+
+  def self.invalidate_cache
+    if cache_store = I18n.cache_store
+      cache_store.delete_matched(/i18n-.*/)
+    end
+  end
 end
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -15,4 +15,6 @@ module I18n
 end
 
 I18n.backend.send(:extend, I18n::Backend::ActiveRecord::Missing)
+I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
+I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n.backend)
