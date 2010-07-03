3a92612a7aed1f278f29db239a5062501244bd5d
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-07-03 22:21:20 +0200

    Trying to install memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ config.log_level = :debug
 # config.logger = SyslogLogger.new
 
 # Use a different cache store in production
-config.cache_store = :mem_cache_store
+config.cache_store = :mem_cache_store, Memcached::Rails.new
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
