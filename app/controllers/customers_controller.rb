d4c9d05f9962bb79f5ec36c05470f72c41b90434
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-07-03 20:22:32 +0200

    Removed customers from project

--- app/controllers/customers_controller.rb
+++
@@ -1,7 +0,0 @@
-class CustomersController < ApplicationController
-  resource_controller
-  actions :all, :except => [:show]
-
-  create.wants.html { redirect_to customers_path }
-  update.wants.html { redirect_to customers_path }
-end
