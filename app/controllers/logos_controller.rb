33cfa94dbba5b7241c0cb8e397c1e25f9f23e84c
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-07-04 14:16:46 +0200

    Logo voting feature

--- app/controllers/logos_controller.rb
+++ app/controllers/logos_controller.rb
@@ -1,13 +1,18 @@
 class LogosController < ApplicationController
   resource_controller
-  actions :index
+  actions :index, :show, :vote
+  before_filter :check_if_not_voted, :except => [:show]
 
   def vote
-    unless cookies[:voted_logo_id]
-      object.vote!
-      cookies[:voted_logo_id] = object.id
-      redirect_to logos_path
-    else
+    object.vote!
+    cookies[:voted_logo_id] = object.id
+    
+    redirect_to logo_path(object)
+  end
+  
+  private
+  def check_if_not_voted
+    if cookies[:voted_logo_id] and Logo.count(:conditions => {:id => cookies[:voted_logo_id]}) > 0
       redirect_to logo_path(cookies[:voted_logo_id])
     end
   end
